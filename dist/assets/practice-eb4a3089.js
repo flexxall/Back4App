f63b5a235fca2623c6ea756efe33488b83cb2fb0
Flexxall <flexxall@live.com>
2023-02-21 10:01:27 -0600

    cleanup

--- dist/assets/practice-eb4a3089.js
+++ dist/assets/practice-eb4a3089.js
@@ -1,9 +1,9 @@
-import{r,j as t}from"./index-6c910a9f.js";function s({hljs:_}){r.useLayoutEffect(()=>{_.highlightAll()},[]);const e=String.raw`
+import{r,j as t}from"./index-81e8d9c0.js";function s({hljs:_}){r.useLayoutEffect(()=>{_.highlightAll()},[]);const e=String.raw`
    ___| |_  ___  _____ __      __ ____ _____ ___
   / __| __|/ _  |  __/ \ \ /\ / // _  |  __// __|
   \__ | |_  (_| | |     \ V  V /  (_| | |   \__ |
   |___/\__|\__,_|_|      \_/\_/  \__,_|_|   |___/
- `,a=` return (
+ `,a=`return (
     <div className="text-slate-300 text-center">
       <pre>
         <code className="rounded-md min-w-[480px] max-w-fit mx-auto my-5 text-start plaintext">
